c623ea3f4a17539017fb4783b90804983fb9cbcb
Ajal333 <ajapoos1999@gmail.com>
2021-03-13 12:55:26 +0530

    final

--- popup.js
+++ popup.js
@@ -1,12 +1,21 @@
-var modal = document.getElementById("modal-content");
+var modal = document.getElementById("qrCodeContainer");
 var elem = document.getElementById("newImg");
+var btnContainer = document.getElementById("downloadBtnContainer");
+var btn = document.getElementById("downloadBtn");
 
 const addImage = (u) => {
   if (elem) elem.parentNode.removeChild(elem);
   if (modal) modal.innerHTML = `<img src=${u} alt="newQr" id="newImg">`;
 };
 
+const addDownload = (u) => {
+  if (btn) btn.parentNode.removeChild(btn);
+  if (btnContainer)
+    btnContainer.innerHTML = `<a href=${u} id="downloadBtn" class="downloadBtn" download>Download</a>`;
+};
+
 chrome.tabs.query({ active: true }, function (tabs) {
-  var final_url = `https://image-charts.com/chart?chl=${tabs[0].url}&choe=UTF-8&chof=.png&chs=150x150&cht=qr`;
+  var final_url = `https://image-charts.com/chart?chl=${tabs[0].url}&choe=UTF-8&chof=.png&chs=150x150&cht=qr&icqrb=82ffd1`;
   addImage(final_url);
+  addDownload(final_url);
 });
